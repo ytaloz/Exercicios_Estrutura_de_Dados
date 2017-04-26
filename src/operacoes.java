82105fc7ade316cc5790d0b5d6f6b6d4fe632d8f
Italo Silva <italo.ecomp@gmail.com>
2017-04-26 00:21:10 -0300

    Esquele da classe responsavel pelas operacoes

---
+++ src/operacoes.java
@@ -0,0 +1,53 @@
+// Classe para receber estruturas e realizar operações. Assinaturas para as soluções da lista 01.
+
+class Operacoes {
+    
+    boolean compara_Listas (LinkList L1, LinkList L2) {
+        
+    }
+    
+    public void concatena_Listas ( Linklist L1, Linklist L2) {
+        
+    }
+    
+    public Linklist separa_Lista ( Linklist L, int key) {
+        
+    }
+    
+    public int topo (LinkStack Pilha){
+        
+    }
+    
+    void concatena_pilhas (LinkStack P1, LinkStack P2){
+        
+    }
+    
+    public void retira_pares(LinkStack Pilha){
+        
+    }
+    
+    public LinkStack copia_pilhas (LinkStack Pilha){
+        
+    }
+    
+    public boolean pilhas_iguais (LinkStack P1, LinkStack P2) {
+        
+    }
+    
+    public LinkQueue copia_fila (LinkQueue Fila){
+        
+    }
+    
+    public void combina_Filas ( LinkQueue F1, LinkQueue F2, LinkQueue F3) {
+        
+    }
+    
+    public void separa_Filas ( LinkQueue F1, LinkQueue F_pares, LinkQueue F_impares) {
+        
+    }
+    
+    public boolean filas_Iguais ( LinkQueue F1, LinkQueue F2) {
+        
+    }
+    
+} // fim da classe Operacoes
